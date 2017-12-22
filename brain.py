c8dac2f64bfee81c45b29a5805ad1619fe92d691
Mitchell Karchemsky <mitch.karchemsky@gmail.com>
2017-12-21 17:33:36 -0800

    Added documentation

--- brain.py
+++ brain.py
@@ -27,31 +27,73 @@ RAND_FN = random.random
 
 
 def remap(OldValue, OldMin, OldMax, NewMin, NewMax):
+    """ Preforms a linear remap of OldValue from an OldRange to a New Range 
+    
+    Keyword Arguments:
+    OldValue -- Value to remap
+    OldMin   -- Minimum value of old range
+    OldMax   -- Maximum value of old range
+    NewMin   -- Minimum value of new range
+    NewMax   -- Maximum value of new range
+    """
     return (((OldValue - OldMin) * (NewMax - NewMin)) / (OldMax - OldMin)) + NewMin
 
 def rgbMap(OldValue):
+    """ Remaps color percentage (0-1 value) to 8 bit values
+    
+    Keyword Arguments:
+    OldValue -- Integer Value to remap. Should be from 0 to 1
+    """
     return remap(OldValue, 0, 1, 0, 255)
 
 def normalX(OldValue):
+    """ Remaps X location to percentage
+    
+    Keyword Arguments:
+    OldValue -- Integer Value to remap. Should be from -BLEED_X to BLEED_X + size[0]
+    """
     return max(0,remap(OldValue, -BLEED_X, size[0] + BLEED_X, 0.0,1.0))
 
 def normalY(OldValue):
+    """ Remaps Y location to percentage
+    
+    Keyword Arguments:
+    OldValue -- Integer Value to remap. Should be from -BLEED_Y to BLEED_Y + size[0]
+    """
     return max(0,remap(OldValue, -BLEED_Y, size[1] + BLEED_Y, 0.0,1.0))
 
 def centerX(tri):
+    """ Calculates X center of a triangle.
+    
+    Keyword Arguments:
+    tri -- a 2x3 array where each element is a coord of the triangle 
+    """
     return (tri[0][0] + tri[1][0] + tri[2][0]) / 3
 
 def centerY(tri):
+    """ Calculates Y center of a triangle.
+    
+    Keyword Arguments:
+    tri -- a 2x3 array where each element is a coord of the triangle 
+    """
     return (tri[0][1] + tri[1][1] + tri[2][1]) / 3
 
-def calculate_gradient(startcolor, endcolor, rows):
+def calculate_gradient(startcolor, endcolor, transition_steps):
+    """ Calculates gradient from startcolor to endcolor with transition_steps 
+    
+    Keyword Arguments:
+    startcolor       -- an rgb tuple of the color to start with
+    endcolor         -- an rgb tuple of the color to end with
+    transition_steps -- number of colors to end up with total. Must be > 3
+    """
+
     gradient = []
     sr, sg, sb = startcolor
-    dr = (endcolor[0] - sr)/(rows-1)
-    dg = (endcolor[1] - sg)/(rows-1)
-    db = (endcolor[2] - sb)/(rows-1)
+    dr = (endcolor[0] - sr)/(transition_steps-1)
+    dg = (endcolor[1] - sg)/(transition_steps-1)
+    db = (endcolor[2] - sb)/(transition_steps-1)
     cr, cg, cb = sr, sg, sb
-    for r in range(rows):
+    for r in range(transition_steps):
         gradient.append((cr,cg,cb))
         cr += dr
         cg += dg
@@ -62,6 +104,18 @@ def calculate_gradient(startcolor, endcolor, rows):
     return gradient
 
 def gen_grid(w,h,b_x,b_y,cell_size,variance,rand_fn):
+    """ Calculates random 2d dot array
+    
+    Keyword Arguments:
+    w         -- width of grid
+    h         -- height of grid
+    b_x       -- bleed in the x dimmension
+    b_y       -- bleed in the y dimmension
+    cell_size -- size of each triangle cell
+    variance  -- variance in each placement
+    rand_fn   -- function to generate random numbers
+    """
+
     w = w + b_x
     h = h + b_y
     half_cell = cell_size * 0.5
